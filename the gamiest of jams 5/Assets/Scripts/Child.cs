529130e156391618afb87abc14e67736b553d5da
Adam <adam.and.math@gmail.com>
2016-04-02 14:07:00 +0200

    Implemented path finding Created Child Prefab Created Task Prefab

--- the gamiest of jams 5/Assets/Scripts/Child.cs
+++ the gamiest of jams 5/Assets/Scripts/Child.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Child : MonoBehaviour
 {
     private const float ScreamRadius = 3;
@@ -10,6 +11,8 @@ public class Child : MonoBehaviour
     [Range(0, 1)]
     public float panic;
     private bool modified;
+    public ChildTask task;
+    private NavMeshAgent navAgent;
 
     public Vector2 Position { get { return new Vector2(transform.position.x, transform.position.z); } }
     public float Panic
@@ -22,13 +25,26 @@ public class Child : MonoBehaviour
         }
     }
 
+    void Awake()
+    {
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
     void Update()
     {
         if (Panic == 1)
         {
-            foreach(var child in Physics.OverlapSphere(transform.position, ScreamRadius).Select(col => col.GetComponent<Child>()).Where(chi => chi != null))
+            foreach (var child in Physics.OverlapSphere(transform.position, ScreamRadius).Select(col => col.GetComponent<Child>()).Where(chi => chi != null))
             {
                 child.Panic += Time.deltaTime * ScreamIncrease;
+            }
+        }
+        else
+        {
+            if (task == null)
+            {
+                task = ChildTask.GetTask();
+                navAgent.SetDestination(task.transform.position);
             }
         }
     }
---
+++ the gamiest of jams 5/Assets/Scripts/ChildTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildTask : MonoBehaviour
+{
+    public static readonly List<ChildTask> Tasks = new List<ChildTask>();
+
+    void OnEnable()
+    {
+        Tasks.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Tasks.Add(this);
+    }
+
+    public static ChildTask GetTask()
+    {
+        return Tasks[Random.Range(0, Tasks.Count)];
+    }
+}
