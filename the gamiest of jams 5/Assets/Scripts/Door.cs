1e64daf24a2de2a08e136b958abb08dd5ff0215c
Adam <adam.and.math@gmail.com>
2016-04-02 14:17:09 +0200

    Made doors solid

---
+++ the gamiest of jams 5/Assets/Scripts/Door.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshObstacle))]
+public class Door : MonoBehaviour
+{
+    private NavMeshObstacle obstacle;
+
+    void Awake()
+    {
+        obstacle = GetComponent<NavMeshObstacle>();
+    }
+
+    public void OpenCloseDoor()
+    {
+        obstacle.enabled = !obstacle.enabled;
+    }
+}
