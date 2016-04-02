da5c7dacf93ca291e2fb37bf2a935063cf2b772c
Adam <adam.and.math@gmail.com>
2016-04-02 12:40:13 +0200

    Added Child and Task Scripts

---
+++ the gamiest of jams 5/Assets/Scripts/Child.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Child : MonoBehaviour
+{
+    private const float ScreamRadius = 3;
+    private const float ScreamIncrease = 0.2F;
+    private const float PanicReduct = 0.1F;
+
+    [Range(0, 1)]
+    public float panic;
+    private bool modified;
+
+    public Vector2 Position { get { return new Vector2(transform.position.x, transform.position.z); } }
+    public float Panic
+    {
+        get { return panic; }
+        set
+        {
+            panic = Mathf.Clamp(value, 0, 1);
+            modified = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Panic == 1)
+        {
+            foreach(var child in Physics.OverlapSphere(transform.position, ScreamRadius).Select(col => col.GetComponent<Child>()).Where(chi => chi != null))
+            {
+                child.Panic += Time.deltaTime * ScreamIncrease;
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (Panic < 1 && !modified)
+        {
+            Panic -= Time.deltaTime * PanicReduct;
+        }
+
+        modified = false;
+    }
+}
---
+++ the gamiest of jams 5/Assets/Scripts/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    public static readonly List<Task> Tasks = new List<Task>();
+
+    void OnEnable()
+    {
+        Tasks.Add(this);
+    }
+
+    void OnDisable()
+    {
+        Tasks.Add(this);
+    }
+}
